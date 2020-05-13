09a33cffcb0667951138aa31f0162a956235a59c
Brian Alexander <balexander04@gmail.com>
2020-05-13 10:19:38 -0500

    Parameterizing amount of likes

--- docker_quickstart.py
+++ docker_quickstart.py
@@ -5,6 +5,9 @@ from instapy import InstaPy, smart_run
 user_name = environ.get("insta_username")
 password = environ.get("insta_password")
 user_names = environ.get("insta_following").split(",")
+amount_of_likes = (
+    int(environ.get("amount_of_likes")) if environ.get("amount_of_likes") else 10
+)
 geckodriver_path = environ.get("geckodriver_path")
 
 insta_bot = InstaPy(
@@ -17,5 +20,5 @@ insta_bot = InstaPy(
 
 if __name__ == "__main__":
     with smart_run(insta_bot):
-        insta_bot.like_by_users(usernames=user_names, amount=10)
-
+        insta_bot.like_by_users(usernames=user_names, amount=amount_of_likes)
+        insta_bot.end()
